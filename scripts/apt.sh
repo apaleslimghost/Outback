4fef7b024404f432c1dc287cda57001b1d1e5dc7
Matt Brennan <matty.b.1000@gmail.com>
2016-12-11 12:13:00 +0000

    actually do all the deps hopefully

--- scripts/apt.sh
+++ scripts/apt.sh
@@ -2,13 +2,14 @@
 
 set -x -e
 
-packages="texlive-base texlive-latex-base texlive-binaries tipa tex-common texlive-fonts-recommended texlive-latex-recommended texlive texlive-xetex latexmk xzdec"
+packages="texlive texlive-xetex latexmk xzdec"
 
 if [ ! -d ~/.apt ] ; then
 	mkdir -p ~/.apt
 	pushd ~/.apt
 	sudo apt-get update
-	sudo apt-get download $packages
+	deps=$(apt-cache depends -i PACKAGE | awk '/Depends:/ {print $2}' | xargs echo) 
+	sudo apt-get download $packages $deps
 	popd
 fi
 
